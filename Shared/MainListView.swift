e27f287e40ad290421807f725fc12fb00ac290f8
kwaneung <kwaneung.kim@outlook.com>
2022-07-19 00:52:55 +0900

    메인 및 보여지는 뷰 수정

--- Shared/MainListView.swift
+++ Shared/MainListView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct MainListView: View {
+    //SwiftUIMemoApp에서 MemoStore를 공유 데이터 등록
+    //뷰가 생성되는 시점에 store속성과 동일한 타입을 가진 인스턴스로 자동 초기화
+    @EnvironmentObject var store: MemoStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
--- Shared/SwiftUIMemoApp.swift
+++ Shared/SwiftUIMemoApp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore() //메모 저장소를 속성으로 저장
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store) //메인리스트 뷰에 등록하면 이어지는 뷰에서 도일한 객체를 쉽게 사용 가능
+            
         }
     }
 }
