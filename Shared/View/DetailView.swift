d6653a357528de9afeaa49428ec5c2555790759e
kwaneung <kwaneung.kim@outlook.com>
2022-07-21 12:38:11 +0900

    새메모 화면 동적으로 편집화면으로 분기 및 연결

--- Shared/View/DetailView.swift
+++ Shared/View/DetailView.swift
@@ -12,6 +12,8 @@ struct DetailView: View {
     
     @EnvironmentObject var store: MemoStore
     
+    @State private var showCoposer = false
+    
     var body: some View {
         VStack {
             ScrollView {
@@ -31,12 +33,26 @@ struct DetailView: View {
         }
         .navigationTitle("메모보기")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showCoposer = true
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showCoposer) {
+            ComposeView(memo: memo)
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(memo: Memo(content: "Hello"))
-            .environmentObject(MemoStore())
+        NavigationView {
+            DetailView(memo: Memo(content: "Hello"))
+                .environmentObject(MemoStore())
+        }
     }
 }
